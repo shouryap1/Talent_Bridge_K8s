435a7ddb1c8ebcf816331d9e69d69d9572e0762f
shouryap1 <shourya.prasad1@gmail.com>
2024-11-17 12:30:06 +0530

    Jenkins update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,16 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        // target: "http://172.16.201.75:8081",
+        // target: "http://rasp-iiitb360:8081",
+        changeOrigin: true,
+      },
+    },
+    host: true, // needed for the Docker Container port mapping to work
+  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,3 +20,18 @@ export default defineConfig({
     },
   },
 })
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         // target: "http://172.16.201.75:8081",
+//         target: "http://rasp-iiitb360:8081",
+//         changeOrigin: true,
+//       },
+//     },
+//     host: true, // needed for the Docker Container port mapping to work
+//   },
+//   plugins: [react()],
+// });
